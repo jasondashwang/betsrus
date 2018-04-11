00f64b071666817e23faeee3a985cebb43940666
Jason Wang <jwang49@cs.brown.edu>
2018-04-11 14:10:21 -0400

    update react

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron'
+import Button from 'react-bootstrap/lib/Button'
 
 // This will be our main component container for the rest of our site
 class App extends Component {
   render () {
     return (
-      <h1>Hello World</h1>
+      	<Jumbotron>
+		  <h1>Hello, world!</h1>
+		  <p>
+		    This is a simple hero unit, a simple jumbotron-style component for calling
+		    extra attention to featured content or information.
+		  </p>
+		  <p>
+		    <Button bsStyle="primary">Learn more</Button>
+		  </p>
+		</Jumbotron>
     );
   }
 }
