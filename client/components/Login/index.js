12753399dfabffc8fc460efcc9a3e03319d7f696
Miles Slack <mkslack@cs.brown.edu>
2018-04-11 14:28:10 -0400

    Built HTML frame for Login/Signup page.

--- client/components/Login/index.js
+++ client/components/Login/index.js
@@ -4,7 +4,33 @@ import React, { Component } from 'react';
 class Login extends Component {
   render () {
     return (
-      <h1>Login Page</h1>
+      <div>
+	      <h1>Login/Signup</h1>
+
+	      <div id='loginBox' class="logHolder">
+	      	<form method="GET" action="/processLog" id="myLogin">
+	      		<p>Username</p>
+	      		<input type="text" id="userLog"/>
+	      		<p>Password</p>
+	      		<input type="text" id="passLog"/>
+	      		<input type="submit" id="subLog" value="Login"/>
+	      	</form>
+	      </div>
+
+	      <div id='signupBox' class="logHolder">
+	      	<form method="GET" action="/processSignup" id="mySignup">
+	      		<p>Email</p>
+	      		<input type="text" id="emailSign"/>
+	      		<p>Username</p>
+	      		<input type="text" id="userSign"/>
+	      		<p>Password</p>
+	      		<input type="text" id="passSign"/>
+	      		<p>Confirm Password</p>
+	      		<input type="text" id="passSign2"/>
+	      		<input type="submit" id="subSign" value="Signup"/>
+	      	</form>
+	      </div>
+      </div>
     );
   }
 }
