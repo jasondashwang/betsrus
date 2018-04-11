4fcb579f895b992bfb53bc3a31e6f6542a919a28
Jason Wang <jwang49@cs.brown.edu>
2018-04-11 14:26:58 -0400

    added nav and jumbotron

--- client/components/App/index.js
+++ client/components/App/index.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron'
-import Button from 'react-bootstrap/lib/Button'
+import Button from 'react-bootstrap/lib/Button';
+
+import NavBar from '../NavBar';
 
 // This will be our main component container for the rest of our site
 class App extends Component {
   render () {
     return (
-      	<Jumbotron>
-		  <h1>Hello, world!</h1>
-		  <p>
-		    This is a simple hero unit, a simple jumbotron-style component for calling
-		    extra attention to featured content or information.
-		  </p>
-		  <p>
-		    <Button bsStyle="primary">Learn more</Button>
-		  </p>
-		</Jumbotron>
+      <div>
+      	<NavBar />
+        <Jumbotron>
+    		  <h1>Hello, world!</h1>
+    		  <p>
+    		    Welcome to Bets-R-Us
+    		  </p>
+    		  <p>
+    		    <Button bsStyle="primary">Learn more</Button>
+    		  </p>
+    	   </Jumbotron>
+      </div>
     );
   }
 }
