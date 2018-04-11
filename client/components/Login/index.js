2ad5b61ca6501cba6d19234e72811176aea826dd
jwang49 <jwang49@cs.brown.edu>
2018-04-11 13:16:55 -0400

    template for signup and login"

---
+++ client/components/Login/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// This will be our main component container for the rest of our site
+class Login extends Component {
+  render () {
+    return (
+      <h1>Login Page</h1>
+    );
+  }
+}
+
+export default Login;
