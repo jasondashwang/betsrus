df89f4ab0ab3e8c6af0090aba5b3332978d159d8
Miles Slack <mkslack@cs.brown.edu>
2018-04-11 16:09:33 -0400

    Changed the format of the forms to react bootstrap logic. Pending further CSS work.

--- client/components/Login/index.js
+++ client/components/Login/index.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import Panel from 'react-bootstrap/lib/Panel';
 import Tab from 'react-bootstrap/lib/Tab';
 import Tabs from 'react-bootstrap/lib/Tabs';
-import FormGroup from 'react-bootstrap/lib/FormFroup';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import Button from 'react-bootstrap/lib/Button';
 
 // This will be our main component container for the rest of our site
 class Login extends Component {
+
+
   render () {
     return (
       <div>
@@ -15,35 +18,40 @@ class Login extends Component {
 	      <Panel>
 		      <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
 			      <Tab eventKey={1} title="Login">
-			      <div id='loginBox' class="logHolder">
-			      	// <form method="GET" action="/processLog" id="myLogin">
 			      	<form>
-			      		<ControlLabel>Username</ControlLabel>
-			      		<FormControl
-			      		 type="text" 
-			      		 id="userLog"/>
-			      		<p>Password</p>
-			      		<input type="text" id="passLog"/>
-			      		<input type="submit" id="subLog" value="Login"/>
+			      		<FormGroup>
+				      		<ControlLabel>Username</ControlLabel>
+				      		<FormControl type="text"/>
+			      		</FormGroup>
+			      		<FormGroup>
+				      		<ControlLabel>Password</ControlLabel>
+				      		<FormControl type="text"/>
+			      		</FormGroup>
+			      		<Button type="submit">Login</Button>
 			      	</form>
-			      </div>
-		      	</Tab>
+		      	  </Tab>
 
-		      <Tab eventKey={2} title="Sign Up">
-		      <div id='signupBox' class="logHolder">
-		      	<form method="GET" action="/processSignup" id="mySignup">
-		      		<p>Email</p>
-		      		<input type="text" id="emailSign"/>
-		      		<p>Username</p>
-		      		<input type="text" id="userSign"/>
-		      		<p>Password</p>
-		      		<input type="text" id="passSign"/>
-		      		<p>Confirm Password</p>
-		      		<input type="text" id="passSign2"/>
-		      		<input type="submit" id="subSign" value="Sign Up"/>
-		      	</form>
-		      </div>
-		      </Tab>
+		        <Tab eventKey={2} title="Sign Up">
+		        	<form>
+			      		<FormGroup>
+				      		<ControlLabel>Email</ControlLabel>
+				      		<FormControl type="text"/>
+			      		</FormGroup>
+			      		<FormGroup>
+				      		<ControlLabel>Username</ControlLabel>
+				      		<FormControl type="text"/>
+			      		</FormGroup>
+			      		<FormGroup>
+				      		<ControlLabel>Password</ControlLabel>
+				      		<FormControl type="text"/>
+			      		</FormGroup>
+			      		<FormGroup>
+				      		<ControlLabel>Confirm Password</ControlLabel>
+				      		<FormControl type="text"/>
+			      		</FormGroup>
+			      		<Button type="submit">Sign Up</Button>
+			      	</form>
+		        </Tab>
 		      </Tabs>
 	      </Panel>
       </div>
