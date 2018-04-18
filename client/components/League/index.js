4ab8c4a42cf2f2234cfbe65fa9fdf8ecd8822d9a
Miles Slack <mileskennethslack@miless-mbp.devices.brown.edu>
2018-04-18 14:44:25 -0400

    Adding in the league pages.

---
+++ client/components/League/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+// This will be our main component container for the rest of our site
+class League extends Component {
+
+
+  render () {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default League;
