ef17550dd67e44665193d61ba62ea0f62445f6dd
Cyrus Twombly <ctwombly@cs.brown.edu>
2018-05-02 14:48:54 -0400

    post path added for changes to user info

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -52,22 +52,41 @@ router.post('/authenticate', (req, res) => {
   });
 });
 
-router.post('/changePassword', (req, res) => {
-  if (req.body.password === undefined) {
-    res.status(404).send();
-  } else {
-    User.findOneAndUpdate({ _id: req.body.userID },
-      {$set: {password: bcrypt.hashSync(req.body.password, 10)}},
-      {new: true})
-    .exec((err, league) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send();
-      } else {
-        res.status(200).send();
-      }
-    });
-  }
-});
+// router.post('/changePassword', (req, res) => {
+//   if (req.body.password === undefined) {
+//     res.status(404).send();
+//   } else {
+//     User.findOneAndUpdate({ _id: req.body.userID },
+//       {$set: {password: bcrypt.hashSync(req.body.password, 10)}},
+//       {new: true})
+//     .exec((err, league) => {
+//       if (err) {
+//         console.error(err);
+//         res.status(500).send();
+//       } else {
+//         res.status(200).send();
+//       }
+//     });
+//   }
+// });
+
+
+router.post('/updateInfo', (req, res) => {
+	if (req.body === undefined) {
+		res.status(404).send();
+	} else {
+		User.findOneAndUpdate({_id: req.body.userID},
+		{$set: {password: bcrypt.hashSync(req.body.password, 10), email: req.body.email,username: req.body.username }},
+		{new: true}).exec((err, data) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send();
+			}
+			else {
+				res.status(200).send();
+			}
+		})
+	}
+})
 
 module.exports = router;
